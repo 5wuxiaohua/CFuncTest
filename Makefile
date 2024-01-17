6ba9cf98e67cee4713a8b6019b078d844e240bff
wuxiaohua <5wuxiaohua@163.com>
2024-01-17 17:50:48 +0800

    test

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 # 编译器
 CC = gcc
+
 # 编译器标志
 CFLAGS += -g -O0 -Wall -Wextra -std=gnu99
+
 # CFLAGS += -Werror
 LDFLAGS += -lrdkafka
 LDFLAGS += -lpthread
+LDFLAGS += -lcares
+LDFLAGS += -lpcap
+
 # 执行make时，带上TARGET参数，例如make TARGET=test
 ifdef TARGET
     SRCS = $(TARGET).c
@@ -13,20 +18,16 @@ else
     TARGET = main
 endif
 
-# 目标文件
-OBJS = $(SRCS:.c=.o)
-
 # 默认目标
-all: $(TARGET)
+all: build/$(TARGET)
 
 # 生成可执行文件
-$(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+build/$(TARGET): $(SRCS) Makefile | build
+	$(CC) $(CFLAGS) $(SRCS) -o $@ $(LDFLAGS)
 
-# 生成目标文件
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@ $(LDFLAGS)
+build:
+	@mkdir -p $@
 
 # 清理生成的文件
 clean:
-	rm -f $(OBJS) $(TARGET)
+	rm -rf build
---
+++ str.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    char result[100];  // 字符数组用于存储拼接后的字符串
+    int chars_written = 0;  // 记录已写入的字符数
+
+    const char *str1 = "Hello";
+    const char *str2 = "World";
+    int int1 = 42;
+    int int2 = 99;
+
+    // 第一次拼接，将字符串 str1 追加到 result
+    chars_written += snprintf(result + chars_written, sizeof(result) - chars_written, "%s", str1);
+
+    // 第二次拼接，将字符串 str2 追加到 result
+    chars_written += snprintf(result + chars_written, sizeof(result) - chars_written, ", %s", str2);
+
+    // 第三次拼接，将整型 int1 追加到 result
+    chars_written += snprintf(result + chars_written, sizeof(result) - chars_written, ", %d", int1);
+
+    // 第四次拼接，将整型 int2 追加到 result
+    chars_written += snprintf(result + chars_written, sizeof(result) - chars_written, ", %d", int2);
+
+
+    // // 第一次拼接，将字符串 str1 追加到 result
+    // chars_written += sprintf(result + chars_written, "%s", str1);
+
+    // // 第二次拼接，将字符串 str2 追加到 result
+    // chars_written += sprintf(result + chars_written, ", %s", str2);
+
+    // // 第三次拼接，将整型 int1 追加到 result
+    // chars_written += sprintf(result + chars_written, ", %d", int1);
+
+    // // 第四次拼接，将整型 int2 追加到 result
+    // chars_written += sprintf(result + chars_written, ", %d", int2);
+
+    // 打印拼接后的字符串
+    printf("Concatenated String: %s\n", result);
+
+    return 0;
+}
